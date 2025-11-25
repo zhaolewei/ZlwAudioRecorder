075adbb4aac3644c99aecebd9bee92ab0759748b
zhaolewei <739043667@qq.com>
2025-11-25 22:17:15 +0800

    support 16 KB page sizes

--- recorderlib/src/main/jni/Application.mk
+++ recorderlib/src/main/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_ABI := armeabi  armeabi-v7a  arm64-v8a  x86  x86_64  mips  mips64
+APP_ABI := armeabi-v7a  arm64-v8a  x86  x86_64
 APP_MODULES := mp3lame
 APP_CFLAGS += -DSTDC_HEADERS
 APP_PLATFORM := android-21
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
